3c5f496543daac6668bda15ae6cb04f11229d009
Tareq35 <a.t.tariqulislam@gmail.com>
2023-02-18 23:56:21 +0600

    updated

---
+++ js-internal/async.js
@@ -0,0 +1,14 @@
+function doSomething() {
+    console.log('third');
+}
+console.log('first');
+console.log('second');
+// doSomething()
+// setTimeout(doSomething);
+// setTimeout(() => { console.log('third') }, 5000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => { console.log('response received'); return response.json() })
+    .then(json => console.log(json))
+console.log('fourth');
+console.log('fifth');
+
